ce73cf589956eaa1b30f8b7912449267f8decaec
Bruno Luís <99904338+BrunoLuisUFPB@users.noreply.github.com>
2023-01-22 23:56:07 -0300

    Create Soma.c

---
+++ Soma.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+
+    int x, y, soma;
+
+    printf("Digite o valor de X: ");
+    scanf("%i", &x);
+
+    printf("Digite o valor de Y: ");
+    scanf("%i", &y);
+
+    soma = 0;
+    soma = x + y;
+
+    printf("SOMA = %i", soma);
+
+    return 0;
+
+}
